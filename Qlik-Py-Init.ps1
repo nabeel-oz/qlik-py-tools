4ebd0832c81bf98909bde068946812ad2fb00087
Nabeel <khnabeelasif@qlik.com>
2018-10-25 13:06:36 +1100

    Powershell scripts
    Powershell scripts for init and start. Also, updated sample app for K-Fold cross validation to use the model name used by the Prediction app.

---
+++ Qlik-Py-Init.ps1
@@ -0,0 +1,18 @@
+Write-Output "Setting up the Python virtual environment..."
+python -m venv $PSScriptRoot\qlik-py-env
+Write-Output "Copying project files to the new directory..."
+xcopy /E /I $PSScriptRoot\generated $PSScriptRoot\qlik-py-env\generated
+xcopy /E /I $PSScriptRoot\core $PSScriptRoot\qlik-py-env\core
+Write-Output "Activating the virtual environment..."
+& $PSScriptRoot\qlik-py-env\Scripts\activate.ps1
+Write-Output "Installing required packages..."
+python -m pip install --upgrade setuptools pip
+pip install grpcio grpcio-tools numpy scipy pandas cython
+pip install pystan==2.17
+pip install fbprophet
+pip install -U scikit-learn
+pip install hdbscan
+pip install -U skater
+Write-Output "Creating a new firewall rule for TCP port 50055..."
+netsh advfirewall firewall add rule name=Qlik-PyTools dir=in action=allow protocol=TCP localport=50055
+Write-Output "All done. Run Qlik-Py-Start.bat to start the SSE Extension Service."
---
+++ Qlik-Py-Start.ps1
@@ -0,0 +1,3 @@
+& $PSScriptRoot\qlik-py-env\Scripts\activate.ps1
+cd $PSScriptRoot\qlik-py-env\core
+python __main__.py
