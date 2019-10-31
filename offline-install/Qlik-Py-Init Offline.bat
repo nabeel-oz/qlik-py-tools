d03b8125ed47b18f12c66e42538a3bfc69402920
Nabeel <khnabeelasif@qlik.com>
2019-10-31 16:25:25 +1100

    Update Qlik-Py-Init Offline.bat

--- offline-install/Qlik-Py-Init Offline.bat
+++ offline-install/Qlik-Py-Init Offline.bat
@@ -16,14 +16,14 @@ echo Installing required packages... & echo.
 pip install --no-index --find-links=offline grpcio grpcio-tools numpy scipy pandas cython joblib==0.11
 pip install --no-index --find-links=offline pystan==2.17
 pip install --no-index --find-links=offline fbprophet==0.4.post2
-pip install --no-index --find-links=offline scikit-learn==0.20.3
-pip install --no-index --find-links=offline setuptools wheel hdbscan==0.8.22
+pip install --no-index --find-links=offline scikit-learn==0.21.3
+pip install --no-index --find-links=offline setuptools wheel hdbscan==0.8.23
 pip install --no-index --find-links=offline skater==1.1.2
 pip install --no-index --find-links=offline spacy==2.1.4
 pip install ./offline/en_core_web_sm-2.1.0.tar.gz
 pip install --no-index --find-links=offline efficient_apriori==1.0.0
 pip install --no-index --find-links=offline tensorflow==1.14.0
-pip install --no-index --find-links=offline keras==2.2.4
+pip install --no-index --find-links=offline keras==2.2.5
 echo.
 echo Creating a new firewall rule for TCP port 50055... & echo.
 netsh advfirewall firewall add rule name="Qlik PyTools" dir=in action=allow protocol=TCP localport=50055
